ac10c0915d93a9610d989745635a1104523340fa
Andrew <andrewmorris847@gmail.com>
2020-04-15 17:06:01 -0500

    menu add

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,13 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '[app-test]',
-  template: `<div>
-    inline template
-  </div>`,
-  styles: [`
-    div {color: red;}
-  `]
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
 
